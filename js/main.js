982d3f4b32570e5918b8482c000b762be98acf6b
IsratJn <israt9626@gmail.com>
2021-09-23 17:33:31 +0600

    Vue instance

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue ({
+    el: '#vue',
+    data: {
+        product: "Socks",
+        description: "A pair of warm, fuzzy socks!"
+    }
+})
+var app2 = new Vue ({
+    el: '#vue2',
+    data: {
+        product: "Coats"
+    }
+})
+//here i created a instance of vue
