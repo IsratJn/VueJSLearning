12cf4f90e3100c8f44989a12cec1820ba83fc6d7
Pathao Ltd <pathaoltd@Pathaos-MacBook-Air.local>
2021-09-27 15:42:12 +0600

    ShopPage

---
+++ ShopPage/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#header',
+    data: {
+        product: "Coffee",
+        description: "Happiness in a cup!",
+        image: "./Assets/pexels-chevanon-photography-312418.jpg",
+        link: "https://tabaqcoffee.com/"
+    }
+})
