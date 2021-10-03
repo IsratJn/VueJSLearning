e85d0f96dff2a1549cfaa509d281ae7fcf406308
IsratJn <israt9626@gmail.com>
2021-10-04 00:39:14 +0600

    Components and CLI

---
+++ Components/src/Input.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+
+        <p>{{msg}}</p>
+        <input type ="text" v-model="message">
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    props:['msg'],
+    data(){
+        return{
+            message:""
+        }
+    }
+}
+</script>
