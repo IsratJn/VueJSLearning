6b1a237091df3b72bae58a3ef1c953b27be7d8df
IsratJn <israt9626@gmail.com>
2021-10-06 02:33:11 +0600

    Looping

---
+++ Looping/script.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        characters: ['Hemu', 'Rupa', 'Majeda Khala', 'Badol'],
+        favorites: [
+            { name: 'Baker Vai', age: 30 },
+            { name: 'Majeda Khala', age: 55 },
+            { name: 'Muhib', age: 20 }
+        ]
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+});
